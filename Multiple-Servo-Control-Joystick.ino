c3f032802d6875fd57130b77599b6cd58d28d153
Mert KILIC <bilgi.programci@gmail.com>
2020-04-23 13:37:59 +0300

    Add files via upload

---
+++ Multiple-Servo-Control-Joystick.ino
@@ -0,0 +1,27 @@
+#include <Servo.h>
+  
+Servo servo1;
+Servo servo2;
+int joyX = 0;
+int joyY = 1;
+  
+int servoVal;
+  
+void setup() 
+{
+  servo1.attach(3);
+  servo2.attach(5);
+}
+  
+void loop()
+{
+  
+  servoVal = analogRead(joyX);
+  servoVal = map(servoVal, 0, 1023, 0, 180);
+  servo1.write(servoVal);
+
+  servoVal = analogRead(joyY);
+  servoVal = map(servoVal, 0, 1023, 70, 180);
+  servo2.write(servoVal);
+  delay(15);  
+}
